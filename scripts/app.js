f4f2d3736fd76e98bcc4ca11f32948d2c054acbf
Hedayatpour2002 <106871002+Hedayatpour2002@users.noreply.github.com>
2022-12-22 22:10:44 +0330

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+var meno = document.getElementById("meno");
+var icon = document.getElementById("icon");
+var icon2 = document.getElementById("icon2");
+
+function myFun() {
+  meno.style.left = "-20px";
+  icon.style.display = "none";
+  icon2.style.display = "block";
+}
+function myFun1() {
+  meno.style.left = "-320px";
+  icon2.style.display = "none";
+  icon.style.display = "block";
+}
